6d54d43991af4140ee1abf2e6584736f1b79d96a
ChenXianyu <1781309730@qq.com>
2020-12-07 22:14:46 +0800

    chang size

--- model.py
+++ model.py
@@ -12,6 +12,7 @@ class Model(nn.Module):
         self.encoder = self.build_encoder()
         self.decoder = self.build_decoder(in_channels=2048)
         self.conv1 = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.Sigmoid()
 
     def build_encoder(self):
         model = FeatureExtractor(self.encoder_name, self.extract_list)
@@ -20,7 +21,7 @@ class Model(nn.Module):
     def build_decoder(self, in_channels=256):
         Dcov = nn.Sequential()
         i = 0
-        while in_channels > 128:
+        while in_channels > 64:
             Dcov.add_module("Con%d" % i,
                             nn.ConvTranspose2d(in_channels=in_channels, out_channels=in_channels // 2, kernel_size=3,
                                                stride=2, padding=1,
@@ -29,11 +30,6 @@ class Model(nn.Module):
             Dcov.add_module("Re%d" % i, nn.ReLU())
             in_channels //= 2
             i += 1
-        Dcov.add_module("Con%d" % i,
-                        nn.ConvTranspose2d(in_channels=in_channels, out_channels=in_channels // 2, kernel_size=3,
-                                           stride=2, padding=1,
-                                           output_padding=1))
-        Dcov.add_module("Sig", nn.Tanh())
         return Dcov
 
     def forward(self, input):
--- try.py
+++ try.py
@@ -1,13 +1,72 @@
-from torch import nn
-from torch.nn import init
-from torch.autograd import Variable
+import os
+
+import matplotlib.pyplot as plt
 import torch
+from PIL import Image
+from torchvision import transforms
+from dataloader import preprocess
+from model import Model
 
-dconv = nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=2, stride=2, padding=1, output_padding=0,
-                           bias=False)
-init.constant(dconv.weight, 1)
-print(dconv.weight)
+loader = transforms.Compose([
+    transforms.ToTensor()])
 
-input = Variable(torch.ones(1, 1, 2, 2))
-print(input)
-print(dconv(input))
+unloader = transforms.ToPILImage()
+
+
+def save_image(tensor, name):
+    dir = 'results'
+    image = tensor.cpu().clone()  # we clone the tensor to not do changes on it
+    image = image.squeeze(0)  # remove the fake batch dimension
+    image = unloader(image)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    image.save(name)
+
+
+if __name__ == '__main__':
+    root_path = '/NAS2020/Share/chenxianyu/PycharmProjects/CS386-ICME2019GC'
+    save_path = os.path.join(root_path, './checkpoints')
+    img1 = Image.open(os.path.join(root_path, 'data/Images/train/1.png'))
+    img1 = preprocess(img1)
+    img1 = img1.reshape((1,) + img1.shape)
+    print(img1.shape)
+    img2 = Image.open(os.path.join(root_path, 'data/Images/val/241.png'))
+    img2 = preprocess(img2)
+    img2 = img2.reshape((1,) + img2.shape)
+    print(img2.shape)
+    smap1 = Image.open(os.path.join(root_path, 'data/ASD_FixMaps/train/1_s.png'))
+    smap1 = preprocess(smap1)
+    smap2 = Image.open(os.path.join(root_path, 'data/ASD_FixMaps/val/241_s.png'))
+    smap2 = preprocess(smap2)
+    net = Model(input_size=(3, 224, 224))
+    net.load_state_dict(torch.load(os.path.join(save_path, 'epoch_{}.pth'.format(60))))
+    result1 = net(img1)
+    print(result1[0])
+    result2 = net(img2)
+    print(result2[0])
+    print("Fuck")
+    save_image(result1, 'train1.png')
+    save_image(result2, 'val1.png')
+    '''
+    plt.figure(figsize=(10, 5))  # 设置窗口大小
+    plt.suptitle('imgage')  # 图片名称
+    plt.subplot(2, 3, 1), plt.title('img1')
+    plt.imshow(img1), plt.axis('off')
+
+    plt.subplot(2, 3, 2), plt.title('origin1')
+    plt.imshow(smap1, cmap='gray'), plt.axis('off')  # 这里显示灰度图要加cmap
+
+    plt.subplot(2, 3, 3), plt.title('img2')
+    plt.imshow(img2), plt.axis('off')
+
+    plt.subplot(2, 3, 4), plt.title('origin2')
+    plt.imshow(smap2, cmap='gray'), plt.axis('off')
+
+    plt.subplot(2, 3, 5), plt.title('result1')
+    plt.imshow(result1, cmap='gray'), plt.axis('off')
+
+    plt.subplot(2, 3, 6), plt.title('result2')
+    plt.imshow(result2, cmap='gray'), plt.axis('off')
+
+    plt.show()
+    '''
